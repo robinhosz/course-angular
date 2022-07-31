9d30b4a9ba925cc72b5d1259b6725b1684520076
José Robson <joserobsonsiqueira23@hotmail.com>
2022-07-31 16:37:54 -0300

    feat: two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { DataComponent } from './components/parent/data/data.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { IfRendeComponent } from './components/if-rende/if-rende.component';
-import { EventsComponent } from './components/events/events.component';
-import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventsComponent } from './components/events/events.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRendeComponent } from './components/if-rende/if-rende.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { DataComponent } from './components/parent/data/data.component';
 import { PipesClassComponent } from './components/pipes-class/pipes-class.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 
 @NgModule({
@@ -25,10 +27,12 @@ import { PipesClassComponent } from './components/pipes-class/pipes-class.compon
     ChangeNumberComponent,
     ListRenderComponent,
     PipesClassComponent,
+    TwoWayBindingComponent,
   ],
   imports: [
-    BrowserModule
-   
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
