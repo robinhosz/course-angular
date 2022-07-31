64456a486a5d5d8b9b401285da1981201f066640
José Robson <joserobsonsiqueira23@hotmail.com>
2022-07-31 14:50:02 -0300

    feat: add directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { DataComponent } from './components/parent/data/data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     DataComponent,
+    DirectivesComponent,
   ],
   imports: [
     BrowserModule
